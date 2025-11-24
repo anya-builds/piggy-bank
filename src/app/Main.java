9a96f151039420df9dbb0fda3dcf0c52b77a3007
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-24 18:34:37 +0530

    bank-app

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,23 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
         System.out.println("Welcome to console bank!");
-        
+        System.out.println("""
+                1) Open Account
+                2) Deposit
+                3) Withdraw
+                4) Transfer
+                5) Account Statement 
+                6) List Accounts
+                7) Search Accounts by Customer Name
+                8) Exit
+                """);
+        System.out.println("CHOOSE: ");
+        String choice=scanner.nextLine().trim();
+        System.out.println("CHOICE: "+choice);
     }
 }
