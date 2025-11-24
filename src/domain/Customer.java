82ecc28f92168206c9f97276a9a3dfca612199d8
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-24 22:01:35 +0530

    bank-app

--- src/domain/Customer.java
+++ src/domain/Customer.java
@@ -4,4 +4,10 @@ public class Customer {
     private String id;
     private String name;
     private String email;
+
+    public Customer(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
--- src/domain/Transaction.java
+++ src/domain/Transaction.java
@@ -10,5 +10,12 @@ public class Transaction {
     private LocalDateTime timestamp;
     private String note;
 
-
+    public Transaction(String id, String note, LocalDateTime timestamp, String accountNumber, Type type, Double amount) {
+        this.id = id;
+        this.note = note;
+        this.timestamp = timestamp;
+        this.accountNumber = accountNumber;
+        this.type = type;
+        this.amount = amount;
+    }
 }
