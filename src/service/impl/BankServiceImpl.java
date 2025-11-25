ef2c0fea70d93d4634b2f6d6b4ae33af91982f78
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-25 23:03:27 +0530

    piggy

--- src/service/impl/BankServiceImpl.java
+++ src/service/impl/BankServiceImpl.java
@@ -1,11 +1,13 @@
 package service.impl;
 
 import domain.Account;
+import repository.AccountRepository;
 import service.BankService;
 
 import java.util.UUID;
 
 public class BankServiceImpl implements BankService {
+    private final AccountRepository accountRepository = new AccountRepository();
 
 
     @Override
@@ -15,7 +17,7 @@ public class BankServiceImpl implements BankService {
         String accountNumber = UUID.randomUUID().toString();
         Account a=new Account(accountNumber, accountType,(double)0,customerId);
         // save
-
+        accountRepository.save(a);
         return "";
     }
 }
