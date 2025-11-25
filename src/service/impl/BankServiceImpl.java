6656e8ce641e0c2e6bb68bb2b1379fc32bfdcd44
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-25 22:42:06 +0530

    piggy

--- src/service/impl/BankServiceImpl.java
+++ src/service/impl/BankServiceImpl.java
@@ -1,19 +1,19 @@
-//package service.impl;
-//
-//import domain.Account;
-//import service.BankService;
-//
-//import java.util.UUID;
-//
-//public class BankServiceImpl implements BankService {
-//
-//
-//    @Override
-//    public String openAccount(String name, String email, String accountType) {
-//        String customerId = UUID.randomUUID().toString();
-//
-//        String accountNumber = UUID.randomUUID().toString();
-//        Account a=new Account(accountNumber, accountType,(double)0,customerId);
-//        return "";
-//    }
-//}
+package service.impl;
+
+import domain.Account;
+import service.BankService;
+
+import java.util.UUID;
+
+public class BankServiceImpl implements BankService {
+
+
+    @Override
+    public String openAccount(String name, String email, String accountType) {
+        String customerId = UUID.randomUUID().toString();
+
+        String accountNumber = UUID.randomUUID().toString();
+        Account a=new Account(accountNumber, accountType,(double)0,customerId);
+        return "";
+    }
+}
